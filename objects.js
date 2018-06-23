55a83e9b9dada10d149ba72281006eabdd6001fd
john argote <argote.john@gmail.com>
2018-06-23 00:39:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
 
 updatePlaylist(playlist,'Phil Ochs',"Here's To The State Of Mississippi")
 
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.Slowdrive;
+  return playlist;
+}

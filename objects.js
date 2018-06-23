65ff8a3e3ddb1614ad46bfcebd3f930f9a75bf1f
john argote <argote.john@gmail.com>
2018-06-23 00:37:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 var playlist = {'Slowdrive':'Alison','My Bloody Valentine':'Sometimes'}
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+updatePlaylist(playlist,)
